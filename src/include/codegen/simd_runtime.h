0d77b205cd1685048e55c29015e269b0ac5af790
Di Wang <diw3@cs.cmu.edu>
2018-04-08 21:12:22 -0400

    implementing vectorized iteration

--- src/include/codegen/simd_runtime.h
+++ src/include/codegen/simd_runtime.h
@@ -17,11 +17,16 @@ namespace peloton {
 
     class SIMDRuntime {
     public:
-      static inline void SIMDAdd4(int32_t *a, int32_t *b, int32_t *c);
-      static inline void SIMDSub4(int32_t *a, int32_t *b, int32_t *c);
-      static inline void SIMDCmpEq4(int32_t *a, int32_t *b, int32_t *c);
-      static inline void SIMDCmpLt4(int32_t *a, int32_t *b, int32_t *c);
-      static inline void SIMDCmpGt4(int32_t *a, int32_t *b, int32_t *c);
+      static inline void SIMDI32Add4(int32_t *a, int32_t *b, int32_t *c);
+      static inline void SIMDI32Sub4(int32_t *a, int32_t *b, int32_t *c);
+      static inline void SIMDI32CmpEq4(int32_t *a, int32_t *b, int32_t *c);
+      static inline void SIMDI32CmpLt4(int32_t *a, int32_t *b, int32_t *c);
+      static inline void SIMDI32CmpGt4(int32_t *a, int32_t *b, int32_t *c);
+
+      static inline void SIMDI16CmpEq8(int16_t *a, int16_t *b, int16_t *c);
+
+      static inline void SIMDI8CmpEq16(int8_t *a, int8_t *b, int8_t *c);
+
       static inline void SIMDFAdd4(float *a, float *b, float *c);
       static inline void SIMDFSub4(float *a, float *b, float *c);
     };
