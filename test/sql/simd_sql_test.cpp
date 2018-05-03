9088ec696296008a76525a8f0a56fb411773e69a
Lin Ma <malin1993ml@gmail.com>
2018-05-02 21:58:07 -0400

    Fix the compilation issue.

--- test/sql/simd_sql_test.cpp
+++ test/sql/simd_sql_test.cpp
@@ -69,7 +69,8 @@ class SIMDSQLTests : public PelotonTest {
     txn = txn_manager.BeginTransaction();
     auto *catalog = catalog::Catalog::GetInstance();
     auto test_db = catalog->GetDatabaseWithName(DEFAULT_DB_NAME, txn);
-    auto test_table = test_db->GetTableWithName("test");
+    auto test_table = catalog->GetTableWithName(
+        DEFAULT_DB_NAME, DEFUALT_SCHEMA_NAME, "test", txn);
     auto *table_schema = test_table->GetSchema();
 
     auto col_val = [](uint32_t tuple_id, uint32_t col_id) {
@@ -161,31 +162,24 @@ class SIMDSQLTests : public PelotonTest {
 
 TEST_F(SIMDSQLTests, SimpleSelectTest) {
   // Testing predicate
-  TestUtil(
-      "SELECT c, a from test where b=21",
-      {"22", "20"}, false);
+  TestUtil("SELECT c, a from test where b=21", {"22", "20"}, false);
 }
 
 TEST_F(SIMDSQLTests, SelectTest1) {
-  TestUtil(
-      "SELECT c, a from test where a<70.0 and b=21",
-      {"22", "20"}, false);
+  TestUtil("SELECT c, a from test where a<70.0 and b=21", {"22", "20"}, false);
 }
 
 TEST_F(SIMDSQLTests, SelectTest2) {
-  TestUtil(
-      "SELECT c, a from test where b=21 and a<70 and c>15 and b>7",
-      {"22", "20"}, false);
+  TestUtil("SELECT c, a from test where b=21 and a<70 and c>15 and b>7",
+           {"22", "20"}, false);
 }
 
 TEST_F(SIMDSQLTests, SelectTest3) {
   // Testing predicate
-  TestUtil(
-      "SELECT c, a from test where (b-2)*3+5*a=(c-2)*8-3 and b=21",
-      {"22", "20"}, false);
-  TestUtil(
-      "SELECT c, a from test where (b-2)*3+5*a=(c-2)*8-3 and b=21",
-      {"22", "20"}, false);
+  TestUtil("SELECT c, a from test where (b-2)*3+5*a=(c-2)*8-3 and b=21",
+           {"22", "20"}, false);
+  TestUtil("SELECT c, a from test where (b-2)*3+5*a=(c-2)*8-3 and b=21",
+           {"22", "20"}, false);
 }
 
 #if 0
